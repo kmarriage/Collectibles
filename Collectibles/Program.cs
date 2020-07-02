15a3f6454bad62ca08f0d8bd458c41ea74fafaeb
Ken <kmarriage@comcast.net>
2020-07-02 16:36:52 -0600

    Add project files.

---
+++ Collectibles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Collectibles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var cards = new GarbagePailKids
+            {
+                Name = "Karate Kate",
+                Manufacturer = "Topps",
+                Series = 3
+            };
+            Console.WriteLine("New card created named " + cards.Name);
+            Console.WriteLine("Made by: " + cards.Manufacturer);
+            Console.WriteLine("In Series: " + cards.Series);
+        }
+    }
+}
